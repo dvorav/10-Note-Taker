f4c732c0141e703438028c531dd15ce02a89653b
dvorav <d.vorav612@gmail.com>
2020-12-18 04:23:42 -0600

    Link to port 3001

--- server.js
+++ server.js
@@ -4,3 +4,31 @@ const fs = require("fs");
 const database = require("./db/db.json");
 const path = require("path");
 const express = require("express");
+
+//Express linkage
+let app = express();
+let PORT = process.env.PORT || 3001;
+
+
+
+
+
+// app.use(express.static('public'));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Notes html and it's "url"
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
