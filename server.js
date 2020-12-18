e59b2ddf0cc518eb90fd07dc195447ed81bbaaf5
dvorav <d.vorav612@gmail.com>
2020-12-18 04:34:11 -0600

    grab from notes list

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const express = require("express");
 let app = express();
 let PORT = process.env.PORT || 3001;
 
-
-
+//grab from notes 
+app.route("/api/notes")
+    // Grab the notes list (this should be updated for every new note and deleted note.)
+    .get(function (req, res) {
+        res.json(database);
+    })
 
 
 app.use(express.static('public'));
@@ -23,12 +27,15 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// Notes html and it's "url"
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
 
+
+
+
+//Starts Server
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
